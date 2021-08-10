7e383c1cf9bd34740f14d2a86cb94c7bff12e7fb
刘灿 <33370733@qq.com>
2021-08-10 19:04:00 +0800

    demo 代码提交

--- src/pages/Hook/PageLayoutHook.js
+++ src/pages/Hook/PageLayoutHook.js
@@ -1,20 +1,64 @@
-import React from 'react'
+import React from 'react';
 import ProLayout, { PageContainer } from '@ant-design/pro-layout';
-import router from "../../config/router"
+// import router from "../../config/router";
+import {Link} from 'react-router-dom'
+import Welcome from "./Welcome";
+import MainClass from './Main';
 
-const PageLayoutHook = () => {
+function getMenu() {
+  const menuData = {
+    route: {
+      path: '/',
+      routes: [
+        {
+          "path": "/",
+          "name": "MainClass 类",
+          "component": MainClass
+        },
+        {
+          "path": "/welcome",
+          "name": "Welcome 类",
+          "component": Welcome
+        }
+      ]
+    },
+    location: {
+      pathname: '/'
+    }
+  }
+  return menuData
+}
+const PageLayoutHook = (props) => {
+  const {children, location = {pathname: '/'}} = props;
+
   return (
     <div id="wapper">
       <ProLayout
-        {...router}
-        title="Ant Design Pro"
+        {...getMenu()}
+        location="/"
+        title="Ant Design"
+        logo='https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg'
         siderWidth="220px"
         fixSiderbar={true}
         fixedHeader={true}
+        menuItemRender={(item, dom) => (
+          <Link to={item.path}>{dom}</Link>
+        )}
+        itemRender={(route, params, routes, paths) => {
+          const first = routes.indexOf(route) === 0;
+          return first ? (
+            <Link to={paths.join('/')}>{route.breadcrumbName}</Link>
+          ) : (
+            <span>{route.breadcrumbName}</span>
+          );
+        }}
       >
-        <PageContainer header="" title="Demo" content="欢迎使用">
-          
-        </PageContainer>
+        <PageContainer 
+          title="我是标题"
+          content = {() => {
+            return <div>Hello</div>
+          }}
+        ></PageContainer>
       </ProLayout>
     </div>
   )
--- src/pages/Hook/Welcome.js
+++ src/pages/Hook/Welcome.js
@@ -1,24 +1,6 @@
-import React, { useEffect } from 'react'
-import { useState } from 'react'
-
 const Welcome = (props) => {
-    const [users, setUsers] = useState([]);
-
-    useEffect(() => {
-        fetch('/api/users').then(resp => resp.json()).then(res => {
-            setUsers(res.data)
-        })
-        return () => { };
-    }, [])
-
     return (
-        <div>
-            PageLayoutHook：
-            <hr />
-            {users.map((item) =>
-                <p key={item.key}>{item.name}</p>
-            )}
-        </div>
+        <div>Welcome</div>
     )
 }
 
